15e110608fbb5150adf54abe52289e15e8b0b3c3
chonlapat323 <cschonlapat@gmail.com>
2025-04-22 16:44:22 +0700

    refactor(address): migrate address module from TypeORM to Prisma

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Address } from './entities/address.entity';
-import { User } from 'src/users/user.entity';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Address, User]), PrismaModule],
+  imports: [PrismaModule],
   controllers: [AddressController],
   providers: [AddressService],
   exports: [AddressService],
