18a957b3e4236ac63b70a73735da1bd872e6ee79
chonlapat323 <cschonlapat@gmail.com>
2025-05-03 23:38:43 +0700

    check/login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcrypt';
 import { LoginUserPayload } from 'types/auth/auth.services';
 import { ConfigService } from '@nestjs/config';
 
