8f0da0e3d32d9c8bcddcccf36c8f7a7ddf75a719
chonlapat323 <cschonlapat@gmail.com>
2025-05-07 16:15:33 +0700

    feat(payment): add AuthModule

--- src/payment-method/payment-method.module.ts
+++ src/payment-method/payment-method.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PaymentMethodService } from './payment-method.service';
 import { PaymentMethodController } from './payment-method.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [PaymentMethodController],
   providers: [PaymentMethodService],
 })
